8ef9928a4e07a3b9ddc2554c8057de1bec69fa40
Everlers <1425295900@qq.com>
2020-04-02 19:26:37 +0800

    20200402-19:26

--- main/st7789v/st7789v.c
+++ main/st7789v/st7789v.c
@@ -1,1 +1,17 @@
 #include "st7789v.h"
+#include "st7789v.h"
+#include "driver/spi_common.h"
+#include "driver/spi_master.h"
+
+void st7789vInit(void)
+{
+    spi_bus_config_t buscfg;
+    spi_device_interface_config_t devcfg;
+    
+    buscfg.miso_io_num = -1;
+    buscfg.quadhd_io_num = -1;
+    buscfg.quadwp_io_num = -1;
+    buscfg.mosi_io_num = GPIO_NUM_19;
+    buscfg.sclk_io_num = GPIO_NUM_18;
+    buscfg.max_transfer_sz = 
+}
---
+++ main/st7789v/st7789v.h
@@ -0,0 +1,7 @@
+#ifndef _ST7789V_H_
+#define _ST7789V_H_
+#include "stdio.h"
+#include "driver/gpio.h"
+
+
+#endif
